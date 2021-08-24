a034e98df73713f67627da83ff1e0acbd8102dbc
SergeitZ <sergiofranco6@gmail.com>
2021-08-24 19:47:17 -0400

    Finished assignment

--- src/GroceryStore/Main.java
+++ src/GroceryStore/Main.java
@@ -7,14 +7,22 @@ public class Main {
 
     public static void main(String[] args) {
 
-        Product prod = new Fruit("Apple", 89, "p-001", "Red Apple", 5, true);
-        Fruit prod2 = new Fruit("Orange", 5_50, "p-002", "Bag of blood oranges", 4);
-        Product prod3 = new Drink("Milk", 4_00 , "d-001", "Whole milk Gallon", 1, "Gal");
-        Drink prod4 = new Drink("Orange Juice", 4_99, "d-002", "with pulp", 2 , 5);
+//        Product prod = new Fruit("Apple", 89, "p-001", "Red Apple", 5, true);
+//        Fruit prod2 = new Fruit("Orange", 5_50, "p-002", "Bag of blood oranges", 4);
+//        Product prod3 = new Drink("Milk", 4_00 , "d-001", "Whole milk Gallon", 1, "Gal");
+//        Drink prod4 = new Drink("Orange Juice", 4_99, "d-002", "with pulp", 2 , 5);
 
-        System.out.println(prod);
-        System.out.println(prod2);
-        System.out.println(prod3);
-        System.out.println(prod4);
+        Store store = new Store("Sergio's Emporium");
+
+        store.addToInventory("Soy boy", 999_000, "001", "soy boy milk", 2, "gal");
+        store.addToInventory("Mega Mango", 6_99, "002", "Mango that's mega", 6, true);
+
+        store.displayInventory();
+
+//        System.out.println(prod);
+//        System.out.println(prod2);
+//        System.out.println(prod3);
+//        System.out.println(prod4);
+
     }
 }
--- src/GroceryStore/Store.java
+++ src/GroceryStore/Store.java
@@ -1,9 +1,8 @@
 package GroceryStore;
-import GroceryStore.Products.Drink;
-import GroceryStore.Products.Fruit;
-import GroceryStore.Products.Product;
+import GroceryStore.Products.*;
 import java.util.ArrayList;
 import java.util.List;
+
 
 public class Store {
     private List<Product> inventory = new ArrayList<>();
@@ -16,18 +15,35 @@ public class Store {
 
     //TODO: add to the given collection
     public void addToInventory(Product product) {
-
+        inventory.add(product);
     }
 
     //TODO: Drink version
-//    public void addToInventory() {
-//        Drink drink = new Drink();
-//        addToInventory(drink);
-//    }
+    public void addToInventory(String name, int price, String id, String description, int volume, String volumeUnit) {
+        Drink drink = new Drink(name, price, id, description, volume, volumeUnit);
+        addToInventory(drink);
+    }
+
+    public void addToInventory(String name, int price, String id, String description, int volume, int volumeUnit) {
+        Drink drink = new Drink(name, price, id, description, volume, volumeUnit);
+        addToInventory(drink);
+    }
 
     //TODO: Fruit version
-//    public void addToInventory() {
-//        Fruit fruit = new Fruit();
-//        addToInventory(fruit);
-//    }
+    public void addToInventory(String name, int price, String id, String description, int hardness) {
+        Fruit fruit = new Fruit(name, price, id, description, hardness);
+        addToInventory(fruit);
+    }
+
+    public void addToInventory(String name, int price, String id, String description, int hardness, boolean isOrganic) {
+        Fruit fruit = new Fruit(name, price, id, description, hardness, isOrganic);
+        addToInventory(fruit);
+    }
+
+    public void displayInventory() {
+        for (Product product : inventory) {
+            System.out.println(product);
+        }
+    }
+
  }
